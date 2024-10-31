0b6749de18176784b179e2107d1f23fea287ddf6
Simone Piccirilli <simonepiccirilli@MBP-di-Simone.lan>
2024-10-31 19:47:45 +0100

    first commit

---
+++ src/main/java/com/the/simone/ia/TheSimoneIaApplication.java
@@ -0,0 +1,13 @@
+package com.the.simone.ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheSimoneIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheSimoneIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/the/simone/ia/TheSimoneIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.the.simone.ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSimoneIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
